d6d2f0d7ea172b318d7d26e04b942eeaa91d2614
Gabriel Vélez Schrod <gabriel.velez@outlook.com>
2015-10-25 01:00:15 +0200

    Modifico el enlace para no utilizar chdir.php si no la propia conexión del fichero

--- home.php
+++ home.php
@@ -83,7 +83,7 @@ echo '
 			if($tamano=="-0.00 Kb") # Si es -0.00 Kb se refiere a un directorio
 			{
 				$tipo="directorio";
-				$objeto="<i><a href='chdir.php?carpeta_actual=$objeto'>".$objeto."</a></i>";
+				$objeto="<i><a href='home.php?carpeta_actual=$objeto'>".$objeto."</a></i>";
 				$tamano="&nbsp;";
 				$fecha="&nbsp;";
 			}
