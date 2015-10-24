3c61b9f7e6b874e941935c9cb3da7d666d97d78d
Gabriel Vélez Schrod <gabriel.velez@outlook.com>
2015-10-25 01:56:39 +0200

    Aislamos el menú superior en un fichero aparte y lo llamamos con un include

---
+++ menu_sup.php
@@ -0,0 +1,33 @@
+<?php
+echo '
+		<table width="30%" border="0" align="center" cellspacing="0" cellpadding="0">
+			<tr>
+				<td>
+					<div align="center">
+						<a href="subida.php" class="button"/><img src="img/upload_file.jpg" title="Subir fichero" width="50" height="50" alt="Subir fichero"></a>
+					</div>
+				</td>
+				<td>
+					<div align="center">
+						<a href="borrar.php" class="button"/><img src="img/borrar.jpg" title="Borrar" width="50" height="50" alt="Borrar"></a>
+					</div>
+				</td>
+				<td>
+					<div align="center">
+						<a href="renombrar.php" class="button"/><img src="img/modificar.jpg" title="Renombrar" width="50" height="50" alt="Renombrar"> </a>
+					</div>
+				</td>
+				<td>
+					<div align="center">
+						<a href="crear.php" class="button"/><img src="img/directorio.jpg" title="Crear directorio" width="50" height="50" alt="Crear directorio"></a>
+					</div>
+				</td>
+				<td>
+					<div align="center">
+						<a href="descargar.php" class="button"/>Descargar</a>
+					</div>
+				</td>
+			<tr>
+		</table>
+';
+?>
