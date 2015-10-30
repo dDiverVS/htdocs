045eed057f4d2f3e8a40446dce6cb5782198eefd
Gabriel Vélez Schrod <gabriel.velez@outlook.com>
2015-10-30 19:02:41 +0100

    Cambio ruta en descargar para Linux

--- descargar2.php
+++ descargar2.php
@@ -1,16 +1,13 @@
 <?php
 include 'seguridad.php';
 include 'conexion.php';
-//elegimos ruta don se puede mejorar descargandon varios jajajajaj renombrar tb querria a varios pero eso luego
-$ruta='C:"'.'xampp"'.'htdocs"'.'.hidden"';
-$ruta2=addslashes($ruta);
-$ruta3=str_replace('"', '', $ruta2);
+
+$ruta='/var/www/.hidden';
 
 if (isset($_POST['id_descargar'])) {
 	$id_descargar=$_POST['id_descargar'];
-	$id_descargar2=str_replace('/', '', $id_descargar);
-	if (ftp_get($conn, $ruta3.$id_descargar2, $id_descargar, FTP_BINARY)) {
-		$fichero=$ruta3.$id_descargar2;
+	if (ftp_get($conn, $ruta.$id_descargar, $id_descargar, FTP_BINARY)) {
+		$fichero=$ruta.$id_descargar;
 		if (file_exists($fichero)) {
 			header('Content-Description: File Transfer');
 			header('Content-Type: application/octet-stream');
@@ -20,15 +17,14 @@ if (isset($_POST['id_descargar'])) {
 			header('Pragma: public');
 			header('Content-Length: ' . filesize($fichero));
 			if (readfile($fichero)) {
-				exec('del "'.$fichero.'" /Q /F');
+				exec('rm "'.$fichero.'"');
 			}
 			else {
-				exec('del "'.$fichero.'" /Q /F');
+				exec('rm "'.$fichero.'"');
 				header ('location: http://localhost/descargar.php');
 			}
 		}
 	}
 	header ('location: http://localhost/descargar.php?descarga="correcta"');
 }
-echo "$ruta3";
 ?>
