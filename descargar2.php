8803c76c5348bc65f12e9a5f5d133f13ef311de7
morguero <morgy22@hotmail.com>
2015-11-01 12:45:00 +0100

    prueba

--- descargar2.php
+++ descargar2.php
@@ -2,7 +2,39 @@
 include 'seguridad.php';
 include 'conexion.php';
 
-$ruta='/var/www/html/.hidden';
+//Version Windows
+$ruta='C:"'.'xampp"'.'htdocs"'.'.hidden"';
+$ruta2=addslashes($ruta);
+$ruta3=str_replace('"', '', $ruta2);
+
+if (isset($_POST['id_descargar'])) {
+	$id_descargar=$_POST['id_descargar'];
+	$id_descargar2=str_replace('/', '', $id_descargar);
+	if (ftp_get($conn, $ruta3.$id_descargar2, $id_descargar, FTP_BINARY)) {
+		$fichero=$ruta3.$id_descargar2;
+		if (file_exists($fichero)) {
+			header('Content-Description: File Transfer');
+			header('Content-Type: application/octet-stream');
+			header('Content-Disposition: attachment; filename="'.basename($fichero).'"');
+			header('Expires: 0');
+			header('Cache-Control: must-revalidate');
+			header('Pragma: public');
+			header('Content-Length: ' . filesize($fichero));
+			if (readfile($fichero)) {
+				exec('del "'.$fichero.'" /Q /F');
+			}
+			else {
+				exec('del "'.$fichero.'" /Q /F');
+				header ('location: http://localhost/descargar.php');
+			}
+		}
+	}
+	header ('location: http://localhost/descargar.php?descarga="correcta"');
+}
+
+
+//version linux
+/*$ruta='/var/www/html/.hidden';
 
 if (isset($_POST['id_descargar'])) {
 	$id_descargar=$_POST['id_descargar'];
@@ -26,5 +58,5 @@ if (isset($_POST['id_descargar'])) {
 		}
 	}
 	header ('location: http://localhost/descargar.php?descarga="correcta"');
-}
+}*/
 ?>
