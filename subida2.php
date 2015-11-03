99e716f77db318e1e991c2a57542c5a04cfe18e1
morguero <morgy22@hotmail.com>
2015-11-03 23:51:40 +0100

    futura opcion para controlar las subidas
    no se si es posible como lo he planteado o deba filtrarse de algun modo
    diferente el tamaño de los ficheros a subir

--- subida2.php
+++ subida2.php
@@ -1,4 +1,8 @@
 <?php
+$max_upload = (int)(ini_get('upload_max_filesize'));
+$max_post = (int)(ini_get('post_max_size'));
+$memory_limit = (int)(ini_get('memory_limit'));
+$upload_mb = min($max_upload, $max_post, $memory_limit);
 echo '<!DOCTYPE html>
 <html>
 	<head>
@@ -15,7 +19,11 @@ echo '<!DOCTYPE html>
 		//Enlaces para acceder a diferentes opciones
 		include 'menu_sup.php';
 $x=0;
+//Opcion a modificar para controlar el tamaño total de las subidas
+/*if (($_FILES["archivo1"]["size"] + $_FILES["archivo2"]["size"] + $_FILES["archivo3"]["size"]) > $upload_mb){ header ("Location: subida.php?tamano=si ");}
+else{*/
 //si el array $_FILES["archivo"] contiene un nombre, osea,  se va a subir ningun fichero:
+
 	if(!empty($_FILES["archivo1"]["name"]))
 	{
 		//almacenamos el nombre temporal del archivo y su nombre y lo subimos a la carpeta actual
@@ -51,7 +59,7 @@ if(!empty($_FILES["archivo3"]["name"]))
 		ftp_quit($conn);
 			if ($upload==true) $x++;
 	}
-
+//}
 	//si la subida del fichero es exitosa, se le indica al usuario con un texto afirmativo.En caso contrario, se le comunica con un texto de error	
 	if ($x!==0) {
 			echo "<h2 align='center'><font color='green'>Se han subido ".$x." ficheros correctamente</h2>";
