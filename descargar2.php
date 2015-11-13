be82adc40db58a1f657cebeb7ed83a80f546f0c7
Gabriel Vélez Schrod <gabriel.velez@outlook.com>
2015-11-13 23:27:16 +0100

    Fuerzo la descarga en descargar2

--- descargar2.php
+++ descargar2.php
@@ -7,7 +7,15 @@ if (isset($_POST['id_descargar'])) {
 	ob_start();
 	$fichero=ftp_get($conn, 'php://output', $id_descargar, FTP_BINARY)
 	ftp_close($conn);
-	if (ob_get_contents()) {
+	if ($datos=ob_get_contents()) {
+		header('Content-Description: File Transfer');
+		header('Content-Type: application/octet-stream');
+		header('Content-Disposition: attachment; filename="'.basename($id_descargar).'"');
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate');
+		header('Pragma: public');
+		header('Content-Length: ' . filesize($datos));
+		readfile($datos);
 		ob_clean();
 		header ('location: ./descargar.php?descarga="correcta"');
 	}
