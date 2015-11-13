f813a1529d4a9d7524cf34ad632512a4a273ba2f
Gabriel Vélez Schrod <gabriel.velez@outlook.com>
2015-11-13 23:08:25 +0100

    Cambio totalmente descargar2.php

--- descargar2.php
+++ descargar2.php
@@ -1,68 +1,19 @@
 <?php
 include 'seguridad.php';
-
-//Version Windows
-if (strpos(php_uname("s"),"Windows")!==false) {
-$ruta='.hidden\\';
-//$ruta2=addslashes($ruta);
-//$ruta3=str_replace('"', '\"', $ruta);
+include 'conexion.php';
 
 if (isset($_POST['id_descargar'])) {
 	$id_descargar=$_POST['id_descargar'];
-	$id_descargar2=str_replace("./"," ", $id_descargar);
-	if (ftp_get($conn, $ruta.$id_descargar2, $id_descargar, FTP_BINARY)) {
-		$fichero=$ruta.$id_descargar2;
-	//	header ('location: ./descargar.php?descarga='.$fichero.'');
-		if (file_exists($fichero)) {
-			header('Content-Description: File Transfer');
-			header('Content-Type: application/octet-stream');
-			header('Content-Disposition: attachment; filename="'.basename($fichero).'"');
-			header('Expires: 0');
-			header('Cache-Control: must-revalidate');
-			header('Pragma: public');
-			header('Content-Length: ' . filesize($fichero));
-		}
-			if (readfile($fichero)) {
-				exec('del "'.$fichero.'" /Q /F');
-				header ('location: ./descargar.php?descarga="correcta"');
-			}
-			else {
-				exec('del "'.$fichero.'" /Q /F');
-				header ('location: ./descargar.php');
-			}
-		}
-	
-}
-}
-else{
-//version linux
-$ruta='/var/www/html/.hidden/';
-
-if (isset($_POST['id_descargar'])) {
-	include 'conexion.php';
-	$id_descargar=$_POST['id_descargar'];
-	$fichero=$ruta.$id_descargar;
-	echo $fichero;
-	if (ftp_get($conn, $fichero, $id_descargar, FTP_BINARY)) {
-		ftp_close($conn);
-		if (file_exists($fichero)) {
-			header('Content-Description: File Transfer');
-			header('Content-Type: application/octet-stream');
-			header('Content-Disposition: attachment; filename="'.basename($fichero).'"');
-			header('Expires: 0');
-			header('Cache-Control: must-revalidate');
-			header('Pragma: public');
-			header('Content-Length: ' . filesize($fichero));
-			if (readfile($fichero)) {
-				exec('rm "'.$fichero.'"');
-				header ('location: ./descargar.php?descarga="correcta"');
-			}
-			else {
-				exec('rm "'.$fichero.'"');
-				header ('location: ./descargar.php');
-			}
-		}
+	ob_start();
+	$fichero=ftp_get($conn, 'php://output', $id_descargar, FTP_BINARY)
+	ftp_close($conn);
+	if (ob_get_contents()) {
+		ob_clean();
+		header ('location: ./descargar.php?descarga="correcta"');
 	}
-}
+	else {
+		exec('rm "'.$fichero.'"');
+		header ('location: ./descargar.php');
+	}
 }
 ?>
