3e4dd109c1acf2eef17c91b3e146e8da6ba19915
morguero <morgy22@hotmail.com>
2015-11-14 12:20:31 +0100

    origen de descargar2

--- descargar2.php
+++ descargar2.php
@@ -1,6 +1,11 @@
 <?php
 include 'seguridad.php';
-include 'conexion.php';
+
+//Version Windows
+if (strpos(php_uname("s"),"Windows")!==false) {
+$ruta='.hidden\\';
+//$ruta2=addslashes($ruta);
+//$ruta3=str_replace('"', '\"', $ruta);
 
 if (isset($_POST['id_descargar'])) {
 	$id_descargar=$_POST['id_descargar'];
@@ -22,6 +27,62 @@ if (isset($_POST['id_descargar'])) {
 	else {
 		exec('rm "'.$fichero.'"');
 		header ('location: ./descargar.php');
+=======
+	$id_descargar2=str_replace("./"," ", $id_descargar);
+	if (ftp_get($conn, $ruta.$id_descargar2, $id_descargar, FTP_BINARY)) {
+		$fichero=$ruta.$id_descargar2;
+	//	header ('location: ./descargar.php?descarga='.$fichero.'');
+		if (file_exists($fichero)) {
+			header('Content-Description: File Transfer');
+			header('Content-Type: application/octet-stream');
+			header('Content-Disposition: attachment; filename="'.basename($fichero).'"');
+			header('Expires: 0');
+			header('Cache-Control: must-revalidate');
+			header('Pragma: public');
+			header('Content-Length: ' . filesize($fichero));
+		}
+			if (readfile($fichero)) {
+				exec('del "'.$fichero.'" /Q /F');
+				header ('location: ./descargar.php?descarga="correcta"');
+			}
+			else {
+				exec('del "'.$fichero.'" /Q /F');
+				header ('location: ./descargar.php');
+			}
+		}
+	
+}
+}
+else{
+//version linux
+$ruta='/var/www/html/.hidden/';
+
+if (isset($_POST['id_descargar'])) {
+	include 'conexion.php';
+	$id_descargar=$_POST['id_descargar'];
+	$fichero=$ruta.$id_descargar;
+	echo $fichero;
+	if (ftp_get($conn, $fichero, $id_descargar, FTP_BINARY)) {
+		ftp_close($conn);
+		if (file_exists($fichero)) {
+			header('Content-Description: File Transfer');
+			header('Content-Type: application/octet-stream');
+			header('Content-Disposition: attachment; filename="'.basename($fichero).'"');
+			header('Expires: 0');
+			header('Cache-Control: must-revalidate');
+			header('Pragma: public');
+			header('Content-Length: ' . filesize($fichero));
+			if (readfile($fichero)) {
+				exec('rm "'.$fichero.'"');
+				header ('location: ./descargar.php?descarga="correcta"');
+			}
+			else {
+				exec('rm "'.$fichero.'"');
+				header ('location: ./descargar.php');
+			}
+		}
+>>>>>>> parent of f813a15... Cambio totalmente descargar2.php
 	}
+}
 }
 ?>
