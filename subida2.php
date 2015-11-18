dbcae1503b225900170f07c422a232b396c959f5
morguero <morgy22@hotmail.com>
2015-11-19 00:00:02 +0100

    subida con echo

--- subida2.php
+++ subida2.php
@@ -15,6 +15,8 @@ echo '<!DOCTYPE html>
 	
 		//Enlaces para acceder a diferentes opciones
 		include 'menu_sup.php';
+		echo "<table width='60%' border='0' align='center' cellspacing='0' cellpadding='2' class='fondotabla'><tr class='fondotabla' >";
+
 		define('MB', 1048576);
 
 //Opcion a modificar para controlar el tamaño total de las subidas
@@ -41,17 +43,12 @@ for ($x=0; $x < 10; $x++) {
 		//almacenamos el nombre temporal del archivo y su nombre y lo subimos a la carpeta actual
 		$file = $_FILES["archivo".$x]["tmp_name"];
 		$base_archivo = basename($_FILES["archivo".$x]["name"]);
-		if (!$upload = ftp_put($conn, $base_archivo, $file, FTP_BINARY)) {
-			$correcto=1;
-		}
+		if ($upload = ftp_put($conn, $base_archivo, $file, FTP_BINARY)) {
+			echo "<tr class='fondotabla' ><td align='center'><font color='green'>Se ha subido </font>".$base_archivo." <font color='green'> correctamente</font></td></tr>";
+		}else echo "<tr class='fondotabla' ><td align='center'><font color='red>No se ha subido </font>".$base_archivo." <font color='red'> correctamente. Compruebe que tiene permiso para subir.</font></td></tr>";
 	}
 }
-if ($correcto==0) {
-	//Angel aquí lo que sea si funcionan todas las subidas
-}
-else {
-	//Aquí lo que sea si falla alguna subida
-}
+
 	
 ?>
 </table>
